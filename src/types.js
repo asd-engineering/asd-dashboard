a2d723df2070a036dbbc8cc369f07cdd91b9d49c
Kelvin Wuite <kwuite@users.noreply.github.com>
2025-07-03 18:47:53 +0200

    docs: add globalSettings views to types

--- src/types.js
+++ src/types.js
@@ -59,6 +59,7 @@
  * @property {boolean|string} [globalSettings.hideViewControl]
  * @property {boolean|string} [globalSettings.hideServiceControl]
  * @property {boolean|string} [globalSettings.showMenuWidget]
+ * @property {{showViewOptionsAsButtons:boolean|string, viewToShow:string}} [globalSettings.views]
  * @property {{enabled:string, loadDashboardFromConfig:string, defaultBoard?:string, defaultView?:string}} [globalSettings.localStorage]
  * @property {Array<Board>} [boards]
  * @property {{widget: {minColumns:number, maxColumns:number, minRows:number, maxRows:number}}} [styling]
